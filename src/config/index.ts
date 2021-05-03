75155578f9a04653f8fd2e09c628185d628b8323
Andrey Krasovsky <krasowsckij@yandex-team.ru>
2021-05-03 22:26:19 +0300

    feat: info command

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,9 +7,19 @@ const createConfig = () => {
     return parseInt(process.env.PORT ?? '5000');
   };
 
+  const getHttpDomain = () => {
+    return process.env.HTTP_DOMAIN ?? '';
+  };
+
+  const getInterval = () => {
+    return 1000;
+  };
+
   return {
     getToken,
     getPort,
+    getHttpDomain,
+    getInterval,
   };
 };
 
--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,17 +1,23 @@
 import {config} from '../config';
 import {createBot} from '../telegraf';
-import 'dotenv';
 
 export const main = () => {
   const bot = createBot(config.getToken());
 
-  bot.hears('token', (ctx) => {
-    ctx.reply(config.getToken());
+  bot.command('info', (ctx) => {
+    ctx.reply(`Ну даров, батёк! Я бот УРОД и мне ПОЕБАТЬ. Запомнил?
+    Ну так если запомнил то не удивляйся что команды мне говорить лень.
+    Скажу лишь, что рано или поздно, но ты станеш "хуемразью дня")`);
+  });
+
+  bot.command('daily', (ctx) => {
+    ctx.reply(`${ctx.from.first_name} тебе интересно кто сегодня "хуемразь дня"???
+    Глупышка, очевидно же что Максим...`);
   });
 
   bot.launch({
     webhook: {
-      domain: 'http://urod-bot.herokuapp.com/',
+      domain: config.getHttpDomain(),
       port: config.getPort(),
     },
   });
