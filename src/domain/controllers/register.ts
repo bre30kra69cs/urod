9c275397c86ff64276d97a959a1946b6306967f2
Andrey Krasovsky <krasowsckij@yandex-team.ru>
2021-05-06 01:41:47 +0300

    feat: init command #12

--- src/domain/controllers/register.ts
+++ src/domain/controllers/register.ts
@@ -4,8 +4,20 @@ const message = () => {
   return 'Ой ой ой, сучечки 😎❤️⚠️ Я бот *УРОД* и теперь в этом чатике я буду выбирать *ПОПУЩЕННОГО ДНЯ*. Делать это буду каждый день с 10 по 11 ч 😧😹👨👨‍🦲👩‍🦰❤️⚠️🐘🐉 Лаба Лаба Даб Даб!';
 };
 
+const messageIsHere = () => {
+  return 'Я и так тут, дурачимбас 😝🤩😂🤣';
+};
+
 const command = createCommand('register', (dm) => async (ctx) => {
   if (!ctx.chat?.id) {
+    return;
+  }
+
+  const chats = await dm.getChats();
+  console.log(chats);
+
+  if (chats.find(({id}) => id === ctx.chat!.id)) {
+    ctx.replyWithMarkdown(messageIsHere());
     return;
   }
 
