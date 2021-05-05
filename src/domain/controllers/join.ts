b95ae06d6c8efa80ebd0cde5f9389eb1bcaa3299
Andrey Krasovsky <krasowsckij@yandex-team.ru>
2021-05-06 02:22:26 +0300

    fix: join

--- src/domain/controllers/join.ts
+++ src/domain/controllers/join.ts
@@ -1,4 +1,4 @@
-import {getName, createCommand} from '../../utils';
+import {getName, getRandomFrom, getResourceUrl, createCommand} from '../../utils';
 
 const message = (author: string) => {
   return `${author}, теперь я могу выбирать тебя *ПОПУЩЕННЫМ* дня...`;
@@ -8,6 +8,19 @@ const command = createCommand('join', (dm) => async (ctx) => {
   const name = getName(ctx.from?.first_name, ctx.from?.last_name, ctx.from?.username);
 
   if (!ctx.chat?.id || !ctx.from?.id || !name) {
+    return;
+  }
+
+  const chatUsers = await dm.getChatUsers(ctx.chat.id);
+
+  if (chatUsers.find((user) => user.id === ctx.from!.id)) {
+    await ctx.replyWithPhoto({
+      source: getRandomFrom([
+        getResourceUrl('fuck.jpeg'),
+        getResourceUrl('fuck1.png'),
+        getResourceUrl('fuck2.jpeg'),
+      ]),
+    });
     return;
   }
 
