1c3d2df9b65596bc2afd6cc415199f9f442cb951
Andrey Krasovsky <krasowsckij@yandex-team.ru>
2021-05-07 00:25:44 +0300

    feat: meme command #2

--- src/domain/controllers/meme.ts
+++ src/domain/controllers/meme.ts
@@ -1,7 +1,7 @@
 import {createCommand, getRandomMax, getRandomFrom} from '../../utils';
 
 const command = createCommand('meme', async ({api, ctx}) => {
-  const page = getRandomMax(100);
+  const page = getRandomMax(30);
   console.log({page});
   const memes = await api.getMemes(page);
   console.log({memes});
