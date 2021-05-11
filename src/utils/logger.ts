5287a443052c0c4780328779bca3d435d89a85fd
Andrey Krasovsky <krasowsckij@yandex-team.ru>
2021-05-12 02:19:55 +0300

    feat: logs #2

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,12 +1,25 @@
 type Source = (...args: unknown[]) => void;
 
 const createLogger = (level: 'error' | 'info', sources: Source[]) => (...args: unknown[]) => {
-  sources.forEach((source) =>
-    source({
-      level,
-      args,
-    }),
-  );
+  sources.forEach((source) => {
+    try {
+      source(
+        JSON.stringify(
+          {
+            level,
+            args,
+          },
+          undefined,
+          2,
+        ),
+      );
+    } catch {
+      source({
+        level,
+        args,
+      });
+    }
+  });
 };
 
 export const info = createLogger('info', [console.log]);
